6e9f5ce0a8afd6abe13de02ebb5c86b572771d09
huyle101 <huyle1097@gmail.com>
2026-03-01 22:44:57 +0700

    init project

---
+++ src/main/java/com/yoot/clinic/YootClinicApplication.java
@@ -0,0 +1,13 @@
+package com.yoot.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YootClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YootClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoot/clinic/YootClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoot.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YootClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
